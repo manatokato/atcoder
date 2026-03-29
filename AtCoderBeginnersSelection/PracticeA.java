7b8a7ac4efd6749fc84379f097cfbec5ce52eb8f
ma-kato1 <mjq@outlook.jp>
2026-03-29 12:33:11 +0900

    [add・del]

---
+++ AtCoderBeginnersSelection/PracticeA.java
@@ -0,0 +1,17 @@
+package AtCoderBeginnersSelection;
+import java.util.Scanner;
+
+public class PracticeA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        String s = sc.next();
+
+        
+        System.out.println(a + b + c + " " + s);
+    }    
+}
